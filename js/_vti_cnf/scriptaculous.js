a5d83c4c2aae103daa5e1e28bf2c865f0679fdc8
Markus Eckstein <markus@markuseckstein.de>
2018-06-02 18:29:45 +0200

    Old website

---
+++ js/_vti_cnf/scriptaculous.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|09 Mar 2008 22:57:00 -0000
+vti_author:SR|BART\\Ecki
+vti_modifiedby:SR|BART\\Ecki
+vti_nexttolasttimemodified:TW|09 Mar 2008 22:57:00 -0000
+vti_timecreated:TR|22 Aug 2008 18:38:13 -0000
+vti_extenderversion:SR|12.0.0.6211
+vti_backlinkinfo:VX|referenzen/referenzen.htm dienstleistungen/solaranlagen/solaranlagen.htm index.htm dienstleistungen/dienstleistungen.htm dienstleistungen/gaswasserinstallation/gaswasser.htm partner/partner.htm dienstleistungen/blechbearbeitung/blechbearbeitung.htm master.dwt kontakt/kontakt.htm dienstleistungen/heizungen/heizungen.htm dienstleistungen/kundendienst/kundendienst.htm
+vti_syncwith_localhost\\h\:\\my web sites\\subsite/h\:/my web sites/subsite:TW|09 Mar 2008 22:57:00 -0000
+vti_cacheddtm:TX|23 Aug 2008 08:47:40 -0000
+vti_filesize:IR|2654
